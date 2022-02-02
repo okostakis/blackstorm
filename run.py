a75f7fa2a48facc456d3e42980a88929ca803a0c
okostakis <okostakis@gmail.com>
2022-02-02 12:18:33 +0200

    testing

---
+++ run.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env pybricks-micropython
+import myLib198 as myLib
+import time
+
+
+# left=myLib.myMotor(port='b',type='large',inverted=False)
+# right=myLib.myMotor(port='c',type='large',inverted=False)
+# c2=myLib.myColorSensor(port=2,sensorType='ev3',mins={'white':100,'red':255,'green':255,'blue':255},maxs={'white':0,'red':0,'green':0,'blue':0})
+# c3=myLib.myColorSensor(port=3,sensorType='ev3',mins={'white':100,'red':255,'green':255,'blue':255},maxs={'white':0,'red':0,'green':0,'blue':0})
+
+    
